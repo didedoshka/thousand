507510c3af39ef547e6462cd96bbd5442b08921d
didedoshka <gorokhov.080805@gmail.com>
2023-03-30 15:07:58 +0300

    renamed thousand_v0 to Thousand_v0

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from thousand.thousand import Thousand, Player
 from gymnasium.envs.registration import register
 
 register(
-    id="thousand-v0",
+    id="Thousand-v0",
     entry_point="thousand:Thousand",
     max_episode_steps=300,
 )
