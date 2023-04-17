32f25e5308ac2f290c2c8b129fb3bfc0cec6dd42
didedoshka <gorokhov.080805@gmail.com>
2023-04-17 11:52:12 +0300

    implemented State class

--- State.py
+++ State.py
@@ -1,5 +1,11 @@
 from Card import Card
 
+
 class State:
-    def __init__(self) -> None:
-        
+
+    def __init__(self, players_cards: list[list[Card]], turn: int) -> None:
+        self.players_cards = players_cards
+        self.turn = turn
+        self.trump = None
+        self.last = None
+        self.cards_on_desk = []
