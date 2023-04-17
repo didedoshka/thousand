317c09126e406942fdf1f5ca8eb59b2e0006178e
didedoshka <gorokhov.080805@gmail.com>
2023-04-17 11:23:31 +0300

    initialized Card class

---
+++ Card.py
@@ -0,0 +1,16 @@
+class Card:
+    ranks = ['9', 'J', 'Q', 'K', '10', 'A']
+    suits = ['♠', '♣', '♦', '♥']
+
+    def __init__(self, card) -> None:
+        self.card = card
+
+    def get_suit(self) -> int:
+        return self.card % 4
+
+    def get_rank(self) -> int:
+        return self.card // 4
+
+    def get_ansi(self) -> str:
+        return self.ranks[self.get_rank()] + self.suits[self.get_suit()]
+
