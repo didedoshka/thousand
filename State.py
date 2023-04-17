e51a2771efdfcd01c92bf4d8bce9c6672e9eb644
didedoshka <gorokhov.080805@gmail.com>
2023-04-17 12:10:42 +0300

    added ansi State representation

--- State.py
+++ State.py
@@ -9,3 +9,17 @@ class State:
         self.trump = None
         self.last = None
         self.cards_on_desk = []
+
+    def get_ansi(self):
+        from textwrap import dedent
+        return dedent(f"""
+            player0: {[s.get_ansi() for s in self.players_cards[0]]} 
+            player1: {[s.get_ansi() for s in self.players_cards[1]]} 
+            player2: {[s.get_ansi() for s in self.players_cards[2]]} 
+
+            turn: player{self.turn}
+            last: {f'player{self.last}' if self.last is not None else 'nobody'}
+            trump: {Card.suits[self.trump] if self.trump is not None else 'nothing'}
+
+            cards_on_desk: {[s.get_ansi() for s in self.cards_on_desk]}
+        """)
