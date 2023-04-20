68e8a6ac061dd505c9927f5a829a960bbd373b71
didedoshka <gorokhov.080805@gmail.com>
2023-04-20 16:01:56 +0300

    changed hash function

--- State.py
+++ State.py
@@ -28,16 +28,24 @@ class State:
             """)
 
     def get_minimal_ansi(self):
-        return textwrap.dedent(f"""{[s.get_ansi() for s in self.players_cards[0]]}{[s.get_ansi() for s in self.players_cards[1]]}{[s.get_ansi() for s in self.players_cards[2]]}tu{self.turn}la{self.last if self.last is not None else -1}tr{Card.suits[self.trump] if self.trump is not None else '-'}{[s.get_ansi() for s in self.cards_on_desk]}""")
+        return f"""{[s.get_ansi() for s in self.players_cards[0]]}{[s.get_ansi() for s in self.players_cards[1]]}{[s.get_ansi() for s in self.players_cards[2]]}tu{self.turn}la{self.last if self.last is not None else '-'}tr{Card.suits[self.trump] if self.trump is not None else '-'}{[s.get_ansi() for s in self.cards_on_desk]}"""
 
     def __hash__(self) -> int:
         h = 0
-        for i in range(3):
-            h ^= hash((tuple(self.players_cards[i]), i))
-        h ^= hash(self.turn)
-        h ^= hash(tuple(self.cards_on_desk))
-        if self.last is not None:
-            h ^= hash(self.last + 100)
-        if self.trump is not None:
-            h ^= hash(self.trump + 1000)
+        for player_cards in self.players_cards:
+            for card in player_cards:
+                h = h * 100 + (card.card + 1)
+
+        h = h * 10 + (self.turn + 1)
+        for card in self.cards_on_desk:
+            h = h * 100 + (card.card + 1)
+        if self.last is None:
+            h = h * 10 + 4
+        else:
+            h = h * 10 + (self.last + 1)
+
+        if self.trump is None:
+            h = h * 10 + 5
+        else:
+            h = h * 10 + (self.trump + 1)
         return h
